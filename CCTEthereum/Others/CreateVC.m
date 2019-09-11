c74ea79c0230872ccfab0f0063706d3458983f71
TT-struggleofoneself <504470358@qq.com>
2019-09-11 11:26:50 +0800

    更新

--- CCTEthereum/Others/CreateVC.m
+++ CCTEthereum/Others/CreateVC.m
@@ -10,7 +10,7 @@
 #import "CCTEtherLib.h"
 
 /**
- 完整的 CCTEtherLib 代码，请加微信：tiantaotaobao 收费400元（ETH+BTC全套库+Demo代码）
+ 完整的 CCTEtherLib 代码，请加微信：tiantaotaobao 收费600元（ETH+BTC全套库+Demo代码）
  代码里不明白的都可以问我，其他区块链相关的问题我们也都可以互相讨论，专注区块链开发，还希望交个朋友。
  **/
 
--- CCTEthereum/Others/TransferVC.m
+++ CCTEthereum/Others/TransferVC.m
@@ -23,7 +23,7 @@
 - (IBAction)transferBtnAction:(id)sender {
     
     /**
-     完整的 CCTEtherLib 代码，请加微信：tiantaotaobao 收费400元（ETH+BTC全套库+Demo代码）
+     完整的 CCTEtherLib 代码，请加微信：tiantaotaobao 收费600元（ETH+BTC全套库+Demo代码）
      代码里不明白的都可以问我，其他区块链相关的问题我们也都可以互相讨论，专注区块链开发，还希望交个朋友。
      **/
     NSString* keystore=@"{\"version\":3,\"id\":\"6229B92D-124A-4ED0-93E4-FE6180C4BC75\",\"x-ethers\":{\"gethFilename\":\"UTC--2019-03-18T11-36-20.0Z--30cfe84083a636f928719456086aa02556c4c7c2\",\"mnemonicCiphertext\":\"a4242317c29547c337a25eae364aad83\",\"mnemonicCounter\":\"b437fd047e6059978a4821afee42fe44\",\"client\":\"ethers\\/iOS\",\"version\":\"0.1\"},\"Crypto\":{\"ciphertext\":\"d557639d9fe162c3fdb25b77a3a2691adff08a88a6a4a6ed51c8770ec31b2e21\",\"cipherparams\":{\"iv\":\"67eb66e81d7f719f6d5c449728325c72\"},\"kdf\":\"scrypt\",\"kdfparams\":{\"r\":8,\"p\":1,\"n\":262144,\"dklen\":32,\"salt\":\"2c642a95926fd3a01cd58db783fcb1777ebef9018363223f93c510e75ad85634\"},\"mac\":\"84b4f226f77e7a3d18d17400dbd4f15e94ad78d620e3593d0ce04406753ce53a\",\"cipher\":\"aes-128-ctr\"},\"address\":\"30cfe84083a636f928719456086aa02556c4c7c2\"}";
